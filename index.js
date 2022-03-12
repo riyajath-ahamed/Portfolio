06862bde4be682c257e5677efd21b221c34d05cb
riyajath-ahamed <riyajatha@gmail.com>
2022-03-12 13:30:54 +0530

    hype one

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let scene= document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
 
-// SWIPER 
+//*************************** SWIPER ******************************//
 let keys = [
     "Aboutme",
     "Skills",
@@ -13,7 +13,7 @@ let keys = [
 ];
 
 let slider = new Swiper('.swiper-container', {
-    slidesPreView: "auto",
+    slidesPerView: "auto",
     spaceBetween: 150,
     centeredSlides: true,
     mousewheel: true,
@@ -25,6 +25,75 @@ let slider = new Swiper('.swiper-container', {
         }
     }
 });
+
+
+
+//**************************SWIPER ANIMATION ********************************//
+
+slider.on("slideChange", function(){
+    gsap.to(".slide-text span", 0.2, {
+        x:"-100px"
+    })
+    gsap.to(".slide-number span", 0.2, {
+        x:"-100px"
+    })
+    gsap.to(".slide-detail span", 0.5, {
+        x:"-1000px"
+    })
+    gsap.to(".slide-detail-facts div", 0.5, {
+        x:"-1000px"
+    })
+    gsap.to(".swiper-slide-active", 0.5, {
+       scale:0.85
+    })
+    gsap.to(".swiper-slide .slide-img", 1, {
+        rotation:20
+     })
+})
+
+slider.on("slideChangeTransitionEnd", function(){
+    gsap.to(".slide-text span", 0.2, {
+        x:0,
+        delay:0.1
+    })
+
+    gsap.to(".slide-text span", 0.2, {
+        x:"100px",
+        
+    })
+
+    gsap.to(".slide-number span", 0.2, {
+        x:0,
+        delay:0.1
+    })
+    gsap.to(".slide-number span", 0.2, {
+        x:"100px",
+        
+    })
+
+    gsap.to(".slide-detail span", 0.5, {
+        x:0
+    })
+    gsap.to(".slide-detail-facts div", 0.5, {
+        x:0
+    })
+    gsap.to(".swiper-slide-active", 0.5, {
+       scale:1,
+       ease: Power4.easeOut
+    })
+    gsap.to(".swiper-slide .slide-img", 1, {
+        rotation:10
+     })
+})
+
+
+
+
+
+
+
+
+
 
 // window.onload = function () {
 //     //functions definition
