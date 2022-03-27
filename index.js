eb29bd9f5aedddff448ce076f538716591bd437c
riyajath-ahamed <riyajatha@gmail.com>
2022-03-28 00:56:28 +0530

    tagcloud

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
+
 let scene= document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
+
+
 
 //*************************** SWIPER ******************************//
 let keys = [
@@ -14,7 +17,7 @@ let keys = [
 
 let slider = new Swiper('.swiper-container', {
     slidesPerView: "auto",
-    spaceBetween: 150,
+    spaceBetween: 10,
     centeredSlides: true,
     mousewheel: true,
     pagination:{
@@ -88,7 +91,38 @@ slider.on("slideChangeTransitionEnd", function(){
 
 
 
+const myTags = [
+    'JavaScript', 'CSS', 'HTML',
+    'C#', 'React Native',
+    'Python', 'Java', 'Photoshop',
+    'React JS', 'Node.js', 'Angular JS',
+    'Illustration', 'MySQL', 'FireBase',
+];
 
+var tagCloud = TagCloud('.content', myTags,{
+
+  // radius in px
+  radius: 250,
+
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'normal',
+  initSpeed: 'normal',
+
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+  
+  // interact with cursor move on mouse out
+  keep: true
+  
+});
+
+//To change the color of text randomly
+var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
 
 
 
